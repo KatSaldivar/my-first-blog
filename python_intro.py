605cfa88fae274eda1ef0fbccdbc615f63b68624
Kat Saldivar <katherinekaufmann1@gmail.com>
2017-05-13 11:36:19 -0700

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,18 @@
+#print("hello world")
+#name = "Katie"
+#if name=="Katie":
+#    print("hello, katie!")
+#elif name == "sonia":
+#    print("hello sonia")
+def hi(name):
+    print("Hi, " + name + "!")
+
+hi("Katie")
+
+girls = ["ximena", "paula", "esperanza"]
+for name in girls:
+    hi(name)
+    print("next girl")
+
+for i in range(1,6):
+    print(i)
